f4d9e52aaac3d834aa9ad648b9991ab58c1ac046
Daniël W. Crompton <daniel.crompton@gmail.com>
2016-01-06 23:53:00 +0100

    update so people can get password reset mails on their personal account

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,4 +15,16 @@ class User < ActiveRecord::Base
   validates :password, presence: true, length: {minimum: 8}, on: :create
   validates :password_confirmation, presence: true, on: :create
 
+  protected
+
+  def send_devise_notification(notification, *args)
+    if notification == :reset_password_instructions
+      message = devise_mailer.send(notification, self, *args)
+      message.deliver_now
+      unless self.privateemail.nil?
+        message.to = self.privateemail
+        message.deliver_now
+      end
+    end
+  end
 end

11878868315cbf09d1cc2a8471896010f543d552
Daniël W. Crompton <daniel.crompton@gmail.com>
2016-01-07 20:45:55 +0100

    saml routing to canvas beta

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def after_sign_in_path_for(resource)
-    'https://oplerno.instructure.com/'
+    'https://oplerno.beta.instructure.com/'
   end
 end
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -10,6 +10,7 @@ set :ssh_options, {
 set :rails_env, 'production'
 
 set :default_env, {
+  'AUTHY_API_KEY' => ENV['AUTHY_API_KEY'],
   'DEVISE_SECRET' => ENV['DEVISE_SECRET'],
   'DEVISE_PEPPER' => ENV['DEVISE_PEPPER'],
   'DB' => 'mysql',

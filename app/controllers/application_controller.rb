7d20282c7d2e69310e71fd265799b391306ecf36
Daniël W. Crompton <daniel.crompton@gmail.com>
2016-01-07 20:33:49 +0100

    forward to Canvas after login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def after_sign_in_path_for(resource)
-    redirect_to 'https://oplerno.instructure.com/' and return
+    'https://oplerno.instructure.com/'
   end
 end
